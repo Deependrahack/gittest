7dd9ac8ec9b0b51fe3a79cff35120488823a74ac
dsingh <deependra.singh@ballisticlearning.com>
2016-05-05 16:12:59 +0530

    test of branch on second repository

--- abc.php
+++ abc.php
@@ -9,4 +9,5 @@ echo "Chnage on a paritcular commit and create a new branch";
 test of the line in second repository
 echo "test"; second test
 change on the first repository 
+edited from second repository branch
 ?>
